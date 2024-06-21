d04d383b236b4c363578a1dfe108a17883af6171
Anurag yadav <anuragyadav622003@gmail.com>
2024-06-21 14:32:05 +0530

    count cart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import {Routes,Route, BrowserRouter} from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import LoginPage from './pages/SignLogin/LoginPage';
 import SignupPage from './pages/SignLogin/SignUpPage';
+import ContactUs from './components/ContactUs/ContactUs';
+
 function App() {
   return (
     <div className="app">
-      <Cart />
+     
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signin" element={<SignupPage />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path = "/contact-us" element ={<ContactUs/>}/>
         <Route path="/order" element={<PlaceOrder />} />
       </Routes>
     </div>
